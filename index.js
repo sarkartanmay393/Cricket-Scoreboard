b8151ba12fb7b82da325a4bb8581fd1a6b6f8334
Tanmay Sarkar <sarkartanmay393@gmail.com>
2022-12-10 16:54:12 +0530

    Added Overs, Run Rate! ✨

--- index.js
+++ index.js
@@ -10,12 +10,45 @@ let teamBRR = 0;
 function incr(val, whichScore) {
     teamAScore += val;
     if (whichScore == "score-var-a") {
-        if ((teamAOvers * 10) % 5 == 0) {
+        teamAScore += val;
+        if (teamAOvers == 0) {
+            teamAOvers += 0.1;
+        } else if ((teamAOvers * 10) % 10 == 0) {
+            teamAOvers += 0.1;
+        } else if ((teamAOvers * 10) % 5 == 0) {
             teamAOvers = Math.round(teamAOvers);
+        } else {
+            teamAOvers += 0.1;
         }
-        teamAOvers += 0.1;
-        teamAOvers.toFixed(1);
+        teamAOvers = +teamAOvers.toFixed(2);
+        teamARR = +(teamAScore / teamAOvers).toFixed(3);
+    } else {
+        teamBScore += val;
+        if (teamBOvers == 0) {
+            teamBOvers += 0.1;
+        } else if ((teamBOvers * 10) % 10 == 0) {
+            teamBOvers += 0.1;
+        } else if ((teamBOvers * 10) % 5 == 0) {
+            teamBOvers = Math.round(teamBOvers);
+        } else {
+            teamBOvers += 0.1;
+        }
+        teamBOvers = +teamBOvers.toFixed(2);
+        teamBRR = +(teamBScore / teamBOvers).toFixed(3);
     }
-    document.getElementById(whichScore).textContent = teamAScore;
+
+    // Score displaying
+    if (whichScore == "score-var-a") {
+        document.getElementById(whichScore).textContent = teamAScore;
+    } else {
+        document.getElementById(whichScore).textContent = teamBScore;
+    }
+
+    // Overs displaying
     document.getElementById("overs-a-val").textContent = teamAOvers;
+    document.getElementById("overs-b-val").textContent = teamBOvers;
+
+    // Run rate displaying
+    document.getElementById("rr-a-val").textContent = teamARR;
+    document.getElementById("rr-b-val").textContent = teamBRR;
 }
